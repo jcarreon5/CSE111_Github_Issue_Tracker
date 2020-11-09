81d2ce0b143e136c1eb8fde06bede96d0c1da6b5
yeeshue99 <35947745+yeeshue99@users.noreply.github.com>
2020-11-08 18:13:01 -0800

    initial commit

---
+++ sql.sql
@@ -0,0 +1,81 @@
+DROP TABLE IF EXISTS developers;
+DROP TABLE IF EXISTS projectmanagers;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS industry;
+DROP TABLE IF EXISTS developerprojects;
+DROP TABLE IF EXISTS customerprojects;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS releases;
+DROP TABLE IF EXISTS issues;
+DROP TABLE IF EXISTS branches;
+DROP TABLE IF EXISTS mergerequests;
+
+CREATE TABLE developers (
+    e_employeeID DECIMAL(10, 0) NOT NULL, 
+    e_username VARCHAR(20) NOT NULL, 
+    e_password VARCHAR(45) NOT NULL, 
+    e_email VARCHAR(45) NOT NULL, 
+    e_createdDate DATETIME NOT NULL
+);
+
+CREATE TABLE projectmanagers (
+    pm_employeeID DECIMAL(10, 0) NOT NULL,
+    pm_projectID DECIMAL(10, 0) NOT NULL
+);
+
+CREATE TABLE customers (
+    c_customerID DECIMAL(10, 0) NOT NULL, 
+    c_industryID DECIMAL(3, 0), 
+    c_name VARCHAR(45), 
+    c_phone VARCHAR(45),
+    c_address VARCHAR(45),
+    c_createdDate DATETIME NOT NULL
+);
+
+CREATE TABLE industry (
+    ind_industryID DECIMAL(3, 0) NOT NULL,
+    ind_industryName VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE developerprojects (
+    dp_employeeID DECIMAL(10, 0) NOT NULL,
+    dp_projectID DECIMAL(10, 0) NOT NULL
+);
+
+CREATE TABLE customerprojects (
+    cp_customerID DECIMAL(10, 0) NOT NULL,
+    cp_projectID DECIMAL(10, 0) NOT NULL
+);
+
+CREATE TABLE projects (
+    p_projectID DECIMAL(10, 0) NOT NULL, 
+    p_projectname VARCHAR(20) NOT NULL, 
+    p_desc VARCHAR(512), 
+    p_managerID DECIMAL(10, 0), 
+    p_createdDate DATETIME NOT NULL,
+    p_lastUpdate DATETIME NOT NULL
+);
+
+CREATE TABLE releases(
+    r_projectID DECIMAL(10, 0) NOT NULL,
+    r_releaseID DECIMAL(10, 0) NOT NULL,
+    r_desc VARCHAR(512)
+);
+
+CREATE TABLE issues(
+    i_projectID DECIMAL(10, 0) NOT NULL,
+    i_issueID DECIMAL(10, 0) NOT NULL,
+    i_desc VARCHAR(512)
+);
+
+CREATE TABLE branches(
+    b_issueID DECIMAL(10, 0) NOT NULL,
+    b_branchID DECIMAL(10, 0) NOT NULL,
+    b_desc VARCHAR(512)
+);
+
+CREATE TABLE mergerequests(
+    mr_branchID DECIMAL(10, 0) NOT NULL,
+    mr_mergeID DECIMAL(10, 0) NOT NULL,
+    mr_desc VARCHAR(512)
+);
